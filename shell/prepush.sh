efaf4d5e56733a439cd38d22e74f436522c2fff7
Carlos Henrique de Araújo <carlinhosmillenium@yahoo.com.br>
2018-10-21 01:27:15 +0200

    feat: Build CI/CD pipiline (#54)
    feat: add azure pipelines CI to the project
    feat: add git hooks to the project
    feat: add devops tasks to the project

--- shell/prepush.sh
+++ shell/prepush.sh
@@ -1,2 +1,3 @@
-if git-branch-is -q -i -r "^feature/|^hotfix/|^bugfix/|^develop"
-then npm run lint-front && npm run build && npm run test ; else echo 'Current branch is not "develop". Skipping pre-push hook...' ; fi
+if git-branch-is -q -i -r "^feature/|^bugfix/|^hotfix/|^release/|^develop|^master"
+then npm run lint:front && npm run build && npm run test ;
+else echo 'Current branch is not included on gitflow. Skipping pre-push hook...' ; fi
---
+++ shell/set-version.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const nbgv = require('nerdbank-gitversioning');
+
+return nbgv.setPackageVersion();
