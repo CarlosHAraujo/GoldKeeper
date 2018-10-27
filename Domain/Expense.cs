3ac1d2ad7f60c616406ec3d2e0e7ded613c5fb8f
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-10-27 02:41:13 +0200

    chore: add new domain

---
+++ Domain/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Expense
+    {
+        public Company Company { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Cost> ExtraCosts { get; set; }
+        public decimal Discount { get; set; }
+        public decimal GrandTotal { get; set; }
+        public IEnumerable<Payment> PaymentMethod { get; set; }
+    }
+}
