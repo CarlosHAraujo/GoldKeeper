57978c1e530868253ea0122507853cf63a72536d
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-02 19:46:20 +0100

    chore: add data project

---
+++ Data/DomainContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DomainContext : DbContext
+    {
+        public DomainContext(DbContextOptions<DomainContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Expense> Expenses { get; set; }
+        public virtual DbSet<Company> Companies { get; set; }
+    }
+}
---
+++ Data/Migrations/20181102183141_AddCompanies.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddCompanies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Company_CompanyId",
+                table: "Expenses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Company",
+                table: "Company");
+
+            migrationBuilder.RenameTable(
+                name: "Company",
+                newName: "Companies");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Companies",
+                table: "Companies",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Companies_CompanyId",
+                table: "Expenses",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Companies_CompanyId",
+                table: "Expenses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Companies",
+                table: "Companies");
+
+            migrationBuilder.RenameTable(
+                name: "Companies",
+                newName: "Company");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Company",
+                table: "Company",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Company_CompanyId",
+                table: "Expenses",
+                column: "CompanyId",
+                principalTable: "Company",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
