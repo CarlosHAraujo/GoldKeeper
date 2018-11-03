26e787e76f833ccb175e2ddd3f2363295e39bbad
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-03 03:47:25 +0100

    chore: post expense logic

--- GoldKeeper/Controllers/ExpenseController.cs
+++ GoldKeeper/Controllers/ExpenseController.cs
@@ -3,6 +3,8 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,21 +21,21 @@ namespace GoldKeeper.Controllers
             _context = context;
         }
 
-        //public async Task<ActionResult<Expense>> Post(int companyId, DateTime date, decimal discount, , CancellationToken cancellationToken)
-        //{
-        //    var company = await _context.Companies.SingleAsync(x => x.Id == companyId);
-        //    var extraCosts = new ExtraCost() { Cost = cost, Value = value };
-        //    var expense = new Expense()
-        //    {
-        //        Company = company,
-        //        Date = date,
-        //        Discount = discount,
-        //        ExtraCosts = extraCosts,
-        //        GrandTotal = grandTotal,
-        //        PaymentMethod = payment,
-        //        Products = products
-        //    };
-        //    var entity = await _context.Expenses.AddAsync(expense, cancellationToken);
-        //}
+        public async Task<ActionResult<Expense>> Post(int companyId, DateTime date, decimal discount, IDictionary<int, decimal> extraCosts, IDictionary<int, decimal> payments, IDictionary<int, KeyValuePair<int, decimal>> items, CancellationToken cancellationToken)
+        {
+            var company = await _context.Companies.SingleAsync(x => x.Id == companyId);
+
+            var expense = new Expense(company, date, discount);
+
+            expense.AddPayments(payments.Select(p => (p.Key, p.Value)));
+
+            expense.AddItems(items.Select(i => (i.Key, i.Value.Value, i.Value.Key)));
+
+            expense.AddExtraCosts(extraCosts.Select(ec => (ec.Key, ec.Value)));
+
+            var entity = await _context.Expenses.AddAsync(expense, cancellationToken);
+
+            return Ok(entity);
+        }
     }
 }
