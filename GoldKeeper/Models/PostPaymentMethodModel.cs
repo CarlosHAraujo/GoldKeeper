9e9ad8b4c89e998d136d74b2f729d1bb1b251660
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 23:24:34 +0100

    feat: payment method controller

---
+++ GoldKeeper/Models/PostPaymentMethodModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoldKeeper.Models
+{
+    public class PostPaymentMethodModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+    }
+}
