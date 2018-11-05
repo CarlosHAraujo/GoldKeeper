322ecf6bc65adac6014cff7e2ba98a2aae60e018
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 20:26:45 +0100

    feat: add company controller

---
+++ GoldKeeper/Controllers/CompanyController.cs
@@ -0,0 +1,35 @@
+﻿using Data;
+using Domain;
+using GoldKeeper.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoldKeeper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly DomainContext _context;
+
+        public CompanyController(DomainContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<Company>> Post(PostCompanyModel model, CancellationToken cancellationToken)
+        {
+            var company = new Company(model.Name);
+
+            var entity = await _context.Companies.AddAsync(company, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Ok(entity.Entity);
+        }
+    }
+}
---
+++ GoldKeeper/Models/PostCompanyModel.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoldKeeper.Models
+{
+    public class PostCompanyModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+    }
+}
