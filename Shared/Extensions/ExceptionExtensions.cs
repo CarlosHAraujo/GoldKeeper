e68f0329dd707c741336d26d3899889bb2b5a5bd
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 20:24:31 +0100

    feat: exception helper extensions

---
+++ Shared/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// Credits: https://stackoverflow.com/questions/2480521/argumentexception-or-argumentnullexception-for-string-parameters
+/// </summary>
+namespace Shared.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Throws when string is null, empty, or consists only of white-space characters.
+        /// </summary>
+        /// <param name="value">The string to check.</param>
+        /// <returns><paramref name="value"/></returns>
+        public static string CheckEmptyOrWhiteSpace(this string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            if (value == string.Empty)
+            {
+                throw new ArgumentException("Argument must not be the empty string.", nameof(value));
+            }
+            return value;
+        }
+    }
+}
