0c0acc8c21e381d5bbd5fce565d6538fe9380cbc
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 20:26:17 +0100

    test: add company domain tests

---
+++ Tests/DomainTest/CompanyTest.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DomainTest
+{
+    public class CompanyTest
+    {
+        [Theory]
+        [MemberData(nameof(GetNameSampleData))]
+        public void Should_ThrowArgumentNullException_When_NameIsMissing(string name)
+        {
+            Assert.Throws<ArgumentNullException>("name", () => new Company(name));
+        }
+
+        public static IEnumerable<object[]> GetNameSampleData()
+        {
+            return Common.GetEmptyOrNullStringSample().Select(x => new object[] { x });
+        }
+    }
+}
