ddabaa7cbcf3c58d519c2a52cebfc19ef9b9dcf2
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 20:25:02 +0100

    chore: add name to company domain

--- Domain/Company.cs
+++ Domain/Company.cs
@@ -1,7 +1,19 @@
-﻿namespace Domain
+﻿using Shared.Extensions;
+
+namespace Domain
 {
     public class Company
     {
+        private Company()
+        {
+        }
+
+        public Company(string name)
+        {
+            this.Name = name.CheckEmptyOrWhiteSpace();
+        }
+
         public int Id { get; private set; }
+        public string Name { get; private set; }
     }
 }
