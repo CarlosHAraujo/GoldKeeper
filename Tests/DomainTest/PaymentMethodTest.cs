30a540ba55bc6fe108eeebf81573910325b7d9d8
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 23:24:09 +0100

    test: payment method

---
+++ Tests/DomainTest/PaymentMethodTest.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DomainTest
+{
+    public class PaymentMethodTest
+    {
+        [Theory]
+        [MemberData(nameof(GetNameSample))]
+        public void Should_Throw_When_NameIsMissing(string name)
+        {
+            var ex = Assert.ThrowsAny<Exception>(() => new PaymentMethod(name));
+            Assert.Contains(ex.GetType(), Common.GetExpectedExceptionsForMissingParameter());
+            Assert.Contains(nameof(name), ex.Message);
+        }
+
+        public static IEnumerable<object[]> GetNameSample()
+        {
+            return Common.GetEmptyOrNullStringSample().Select(x => new object[] { x });
+        }
+    }
+}
