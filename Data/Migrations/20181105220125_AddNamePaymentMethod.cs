0d7a3c013549a6ea386b848c27f0b6db961f1595
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 23:05:04 +0100

    chore: add payment method name migrations

---
+++ Data/Migrations/20181105220125_AddNamePaymentMethod.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddNamePaymentMethod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PaymentMethods",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PaymentMethods");
+        }
+    }
+}
