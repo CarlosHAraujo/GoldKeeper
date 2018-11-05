686a7edad324504c1f9e6f1f0eec5dc5f1d39cde
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 22:58:59 +0100

    chore: add name to payment method

--- Domain/PaymentMethod.cs
+++ Domain/PaymentMethod.cs
@@ -1,7 +1,19 @@
-﻿namespace Domain
+﻿using Shared.Extensions;
+
+namespace Domain
 {
     public class PaymentMethod
     {
+        private PaymentMethod()
+        {
+        }
+
+        public PaymentMethod(string name)
+        {
+            Name = name.CheckNullOrWhiteSpace();
+        }
+
         public int Id { get; private set; }
+        public string Name { get; private set; }
     }
 }
