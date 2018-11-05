8f823e5f278a0b463d45858b3a8e54aee1b5f411
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 03:54:33 +0100

    test: add expensecontroller

---
+++ Tests/GoldKeeperTest/ControllerTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldKeeperTest
+{
+    public class ControllerTestBase : IDisposable
+    {
+        public readonly DomainContext context;
+
+        public ControllerTestBase()
+        {
+            var options = new DbContextOptionsBuilder<DomainContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            context = new DomainContext(options);
+            context.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
