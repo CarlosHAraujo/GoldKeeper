6112d82aa85361e99d5e8efe73a7081bad82a5a2
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 23:47:55 +0100

    test: payment method controller

---
+++ Tests/GoldKeeperTest/PaymentMethodControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using GoldKeeper.Controllers;
+using GoldKeeper.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GoldKeeperTest
+{
+    public class PaymentMethodControllerTest : ControllerTestBase
+    {
+        [Theory]
+        [MemberData(nameof(GetPostPaymentMethodSample))]
+        public async Task Should_ReturnSuccessResult_When_ModelIsValid(PostPaymentMethodModel postedData)
+        {
+            var controller = new PaymentMethodController(context);
+
+            var result = await controller.Post(postedData, default(CancellationToken));
+
+            var actionResult = Assert.IsType<ActionResult<PaymentMethod>>(result);
+            var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            var paymentMethod = Assert.IsType<PaymentMethod>(okResult.Value);
+            Assert.Equal(postedData.Name, paymentMethod.Name);
+        }
+
+        public static IEnumerable<object[]> GetPostPaymentMethodSample()
+        {
+            yield return new object[] { new PostPaymentMethodModel { Name = "Dinheiro" } };
+        }
+    }
+}
