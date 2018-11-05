74273c1b4c031756a6d47804bb828cd01162e1be
Carlos Araujo <carlinhosmillenium@yahoo.com.br>
2018-11-05 20:32:44 +0100

    feat: add company name migrations

---
+++ Data/Migrations/20181105193049_AddCompanyName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddCompanyName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Companies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Companies");
+        }
+    }
+}
